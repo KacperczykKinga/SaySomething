ac1af97ac1b0ce1e9a93be9e170c52d770faba0e
KacperczykKinga <agnikkac@gmail.com>
2023-10-12 22:48:27 +0200

    add ngModel option to star-rating

--- src/app/templates/star-rating/star-rating.component.ts
+++ src/app/templates/star-rating/star-rating.component.ts
@@ -1,22 +1,54 @@
-import { Component } from "@angular/core";
+import { Component, forwardRef } from "@angular/core";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     selector: 'blogs-star-rating',
-    templateUrl: './star-rating.component.html'
+    templateUrl: './star-rating.component.html',
+    providers: [
+        {
+          provide: NG_VALUE_ACCESSOR,
+          useExisting: forwardRef(() => StarRatingComponent),
+          multi: true
+        }
+      ]
 })
-export class StarRatingComponent {
+export class StarRatingComponent implements ControlValueAccessor{
     ratesArray = [1, 2, 3, 4, 5];
-    chosenRate = 0;
+    chosenRateValue = 0;
+    onChange: any = () => {}
+    onTouch: any = () => {}
 
     onStarClick(clickedStar: number): void {
         this.chosenRate = clickedStar;
     }
 
     showIcon(rate: number) {
-        if(rate <= this.chosenRate) {
+        if(rate <= this.chosenRateValue) {
             return 'star'
         } else {
             return 'star_border'
         }
     }
+
+    set chosenRate(val: number){
+        if( val !== undefined && this.chosenRateValue !== val){
+        this.chosenRateValue = val
+        this.onChange(val)
+        this.onTouch(val)
+        }
+       
+      }
+    
+      writeValue(value: number): void{
+        this.chosenRate = value
+      }
+    
+      registerOnChange(fn: any): void{
+        this.onChange = fn
+      }
+    
+      registerOnTouched(fn: any): void{
+        this.onTouch = fn
+      }
+    
 }
